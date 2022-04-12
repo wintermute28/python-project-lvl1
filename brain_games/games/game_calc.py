9ca0ae76ba767029a9c99fc99a98ba6d633c496e
wintermute28 <a28c@yandex.ru>
2022-04-12 21:58:17 +0300

    edit game_calc.py

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -1,20 +1,20 @@
 from random import randint
 from random import choice
 DESCRIPTION = 'What is the result of the expression?'
+LOWER_LIMIT = 1
+UPPER_LIMIT = 50
 
 
 def round():
+    ramdom_number1 = randint(LOWER_LIMIT, UPPER_LIMIT)
+    ramdom_number2 = randint(LOWER_LIMIT, UPPER_LIMIT)
     opetator_set = "+-*"
-    random_number1 = randint(1, 50)
-    random_number2 = randint(1, 50)
     random_operator = choice(opetator_set)
     if random_operator == "+":
-        question = f"{random_number1} + {random_number2}"
-        right_answer = str(random_number1 + random_number2)
+        right_answer = str(ramdom_number1 + ramdom_number2)
     elif random_operator == "-":
-        question = f"{random_number1} - {random_number2}"
-        right_answer = str(random_number1 - random_number2)
+        right_answer = str(ramdom_number1 - ramdom_number2)
     else:
-        question = f"{random_number1} * {random_number2}"
-        right_answer = str(random_number1 * random_number2)
+        right_answer = str(ramdom_number1 * ramdom_number2)
+    question = f"{ramdom_number1} {random_operator} {ramdom_number2}"
     return question, right_answer
